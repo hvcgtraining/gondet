6300e49de6fa18cd3f27dafab4ceb4f8e3c44030
nguyenchinhhiep <nguyenchinhhiep95@gmail.com>
2019-11-12 11:39:06 +0700

    add

--- dist/js/header.js
+++ dist/js/header.js
@@ -1,28 +1,37 @@
 "use strict";
 
 $(document).ready(function () {
+  // Open menu
   $("#isShowMenu").click(function () {
     $("#navbarSupportedContent").addClass("showMenu");
     $(".bg-body").addClass("show-bg-body");
     $("body").css('overflow', 'hidden');
-  });
+  }); // Close menu
+
   $("#close-menu").click(function (e) {
     $("#navbarSupportedContent").removeClass("showMenu");
     $(".bg-body").removeClass("show-bg-body");
     $("body").css('overflow', 'auto');
-  });
+    $('.op-menu').removeClass("clo-menu");
+  }); //Open Search Mobile
+
   $("#icon-search-mobile").click(function () {
     $("#form-search-mobile").addClass("showSearchMobile");
     $(".bg-body").addClass("show-bg-body");
     $("body").css('overflow', 'hidden');
   });
+  $('.op-menu').click(function () {
+    $(this).addClass('clo-menu');
+  });
   var menu = document.getElementById("navbarSupportedContent");
-  var showBgBody = document.getElementsByClassName("bg-body");
+  var showBgBody = document.getElementsByClassName("bg-body"); // Close All Event Click
+
   $(window).click(function (e) {
     if (event.target.className == "bg-body show-bg-body") {
       $(".bg-body").removeClass("show-bg-body");
       $("#navbarSupportedContent").removeClass("showMenu");
       $('#form-search-mobile').removeClass("showSearchMobile");
+      $('.op-menu').removeClass("clo-menu");
       $("body").css('overflow', 'auto');
     }
   });
